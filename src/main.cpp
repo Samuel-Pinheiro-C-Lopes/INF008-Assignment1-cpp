566a6bf3f3dddd63c4644d1644b8a656e20e7921
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-05-30 00:08:48 -0300

    Added .h files from the new headers and added basic small test for the CourseEvent class initialization and print self method

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
+#include<string>
 
 #include"entity.h"
 #include"event.h"
 #include"participant.h"
+#include"course_event.h"
+#include"student_participant.h"
 
 int main (int argc, char* argv[]) {
     std::cout << "Hello World!" << std::endl;
+    CourseEvent ce("test", 10, "20/10/2024");
+    ce.printSelf();
+
 }
