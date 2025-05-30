26a25cb983aa5c2e745b854fbab7931c663481d5
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-05-30 00:05:38 -0300

    Added comments to entity

--- include/entity/entity.h
+++ include/entity/entity.h
@@ -10,10 +10,20 @@ private:
 protected:
     Entity(int id, std::string name) : id(id), name(name) {};
     ~Entity() {};
+    /* Id sequence for concrete
+     * implementations */
     virtual int nextId() = 0;
 public:
+    /* A getter for the id can be handy
+     * once we use STL containers and
+     * need to find unique elements
+     * or assign a unique key */
     int getId() const;
-    std::string getName() const;
+    /* All entities shall implement
+     * an own way of printing itself -
+     * although I think most
+     * will use ascendant classes
+     * implementations of this */
     virtual void printSelf() const;
 };
 
