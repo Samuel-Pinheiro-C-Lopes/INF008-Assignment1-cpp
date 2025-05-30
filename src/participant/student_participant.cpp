fb22365956c8792a8e6663f3137d2300fac86c0d
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-05-30 00:06:31 -0300

    Added cpp file for StudentParticipant with default id sequence and a printself base implementation

---
+++ src/participant/student_participant.cpp
@@ -0,0 +1,14 @@
+#include"student_participant.h"
+#include"participant.h"
+
+#include<iostream>
+
+/* Default id sequence */
+int StudentParticipant::currentId = 0;
+int StudentParticipant::nextId() {
+    return StudentParticipant::currentId++;
+}
+
+void StudentParticipant::printSelf() const {
+    Participant::printSelf();
+}
