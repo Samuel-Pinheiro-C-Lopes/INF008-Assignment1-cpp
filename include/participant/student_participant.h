d1672b3e1fad4c901613502dd750912e9fe9cd11
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-06-01 16:27:55 -0300

    Added Subject selection method for input constructor

--- include/participant/student_participant.h
+++ include/participant/student_participant.h
@@ -2,14 +2,20 @@
 #define STUDENT_PARTICIPANT_H
 
 #include"participant.h"
+#include"subject.h"
+
+#include<memory>
+#include<unordered_map>
 
 class StudentParticipant : public Participant {
 public:
+    StudentParticipant(const std::unordered_map<int, std::shared_ptr<Subject>>& availableSubjects) : Participant(nextId()), learningSubjects(Subject::selectSubjectsFromInput("Select the subjects the student studies:", availableSubjects)) {};
     StudentParticipant(std::string name, std::string cpf) : Participant(nextId(), name, cpf) {};
     void printSelf() const override;
     int nextId() override;
 private:
     static int currentId;
+    std::unordered_map<int, std::shared_ptr<Subject>> learningSubjects;
 };
 
 #endif
