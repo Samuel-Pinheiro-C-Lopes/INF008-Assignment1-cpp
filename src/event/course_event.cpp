df62aac1e29ac29875ae519af4309b5ac38bf6ff
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-06-01 01:11:17 -0300

    Added course_event header implementation

--- src/event/course_event.cpp
+++ src/event/course_event.cpp
@@ -2,6 +2,10 @@
 
 #include"event.h"
 #include"student_participant.h"
+#include"professor_participant.h"
+#include"subject.h"
+
+#include<iostream>
 
 /* Default id sequence */
 int CourseEvent::currentId = 0;
@@ -9,8 +13,27 @@ int CourseEvent::nextId() {
     return CourseEvent::currentId++;
 }
 
+void CourseEvent::printSelf() const {
+    Event::printSelf();
+    std::cout << "Professor: " << std::endl;
+    this->professor->printSelf();
+    std::cout << "Subject: " << std::endl;
+    this->subject->printSelf();
+}
+
+CourseEvent::CourseEvent(std::string name, int vacancies, std::string date, ProfessorParticipant* professor, Subject* subject) : Event(nextId(), name, vacancies, date), professor(CourseEvent::validateProfessor(professor, subject) ? professor : nullptr ), subject(subject) {
+    if (this->professor == nullptr) {
+        std::cout << "The professor provided wasn't valid for the course event." << std::endl;
+        professor->printSelf();
+    }
+};
+
 /* Should only receive
  * StudentParticipants */
-bool CourseEvent::registerParticipant(std::shared_ptr<StudentParticipant> participant) {
+bool CourseEvent::registerParticipant(StudentParticipant* participant) {
     return Event::registerParticipant(participant);
+}
+
+bool CourseEvent::validateProfessor(ProfessorParticipant* professor, Subject* subject) {
+    return professor->teaches(subject);
 }
