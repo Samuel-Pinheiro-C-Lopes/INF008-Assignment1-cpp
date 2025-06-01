5392b1ffa2baebc7f29b069d669e53861d3c05ed
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-06-01 01:05:01 -0300

    Added professor participant with subjects and behaviors to check subjects and add

--- include/participant/professor_participant.h
+++ include/participant/professor_participant.h
@@ -2,12 +2,21 @@
 #define PROFESSOR_PARTICIPANT_H
 
 #include"participant.h"
+#include"subject.h"
+
+#include<unordered_map>
 
 class ProfessorParticipant : public Participant {
 public:
     void printSelf() const override;
+    bool teaches(Subject* subject) const;
+    bool addSubject(Subject* subject);
 protected:
     ProfessorParticipant(int id, std::string name, std::string cpf) : Participant(id, name, cpf) {};
+private:
+    std::unordered_map<int, Subject*> subjects;
+    int nextId() override;
+    static int currentId;
 };
 
 #endif
