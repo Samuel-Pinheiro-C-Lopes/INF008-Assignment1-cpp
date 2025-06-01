774337498a493398c9934560f950b6679e85f225
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-06-01 01:02:09 -0300

    Course event with subject, professor and validation added

--- include/event/course_event.h
+++ include/event/course_event.h
@@ -3,18 +3,23 @@
 
 #include"event.h"
 #include"student_participant.h"
+#include"professor_participant.h"
 
 #include<string>
 #include<memory>
 
 class CourseEvent : public Event<StudentParticipant> {
 public:
-    bool registerParticipant(std::shared_ptr<StudentParticipant> participant) override;
-    CourseEvent(std::string name, int vacancies, std::string date) : Event(nextId(), name, vacancies, date) { };
+    bool registerParticipant(StudentParticipant* participant) override;
+    CourseEvent(std::string name, int vacancies, std::string date, ProfessorParticipant* professor, Subject* subject);
+    void printSelf() const override;
 protected:
     int nextId() override;
 private:
     static int currentId;
+    Subject* subject;
+    ProfessorParticipant* professor;
+    static bool validateProfessor(ProfessorParticipant* professor, Subject* subject);
 };
 
 #endif
