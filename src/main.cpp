a3e21232e3d98672bd7e7f11087e92dfd22bc6c2
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-06-01 01:13:10 -0300

    Updated base test and added headers

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,11 @@
 #include"course_event.h"
 #include"student_participant.h"
 #include"professor_participant.h"
+#include"university.h"
 
 int main (int argc, char* argv[]) {
     std::cout << "Hello World!" << std::endl;
-    CourseEvent ce("test", 10, "20/10/2024");
+    CourseEvent ce("test", 10, "20/10/2024", nullptr, nullptr);
     ce.printSelf();
 
 }
