39e52dfeb27a04894973eda66e19cbb6f5cb6bc1
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-06-01 01:02:34 -0300

    CPF validation added for participants

--- include/participant/participant.h
+++ include/participant/participant.h
@@ -3,13 +3,21 @@
 
 #include"entity.h"
 #include<string>
+#include<vector>
 
 class Participant : public Entity {
 private:
     const std::string cpf;
 public:
+    static bool validateCPF(std::string cpf, bool verbose);
     void printSelf() const override;
 protected:
-    Participant(int id, std::string name, std::string cpf) : Entity(id, name) {};
+    Participant(int id, std::string name, std::string cpf);
+private:
+    /* constexpr says it's evaluated at compiler time, meaning computation
+     * can be handled by it - it's a good idiomatic practice, as it seems */
+    static constexpr int VERIFIER_START = 9;
+    static constexpr int VERIFIER_MULTIPLIER = 2;
+    static char calculateVerifierDigit(std::vector<char>::iterator rbegin, std::vector<char>::iterator rend);
 };
 #endif
