c8884f4f887da3cb7afe8df849d0ecb4147b35ea
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-06-02 01:25:30 -0300

    Added external participant

---
+++ include/participant/external_participant.h
@@ -0,0 +1,20 @@
+#ifndef EXTERNAL_PARTICIPANT_H
+#define EXTERNAL_PARTICIPANT_H
+
+#include"participant.h"
+
+#include<string>
+
+class ExternalParticipant : public Participant {
+public:
+    ExternalParticipant() : Participant(nextId()), originUniversity(originUniversityFromInput()) {};
+    ExternalParticipant(std::string name, std::string cpf, std::string originUniversity) : Participant(nextId(), name, cpf), originUniversity(originUniversity) {};
+    void printSelf() const override;
+private:
+    static int currentId;
+    int nextId() override;
+    std::string originUniversity;
+    std::string originUniversityFromInput();
+};
+
+#endif
---
+++ src/participant/external_participant.cpp
@@ -0,0 +1,23 @@
+#include "external_participant.h"
+
+#include"participant.h"
+
+#include<iostream>
+#include<string>
+
+/* Default id sequence */
+int ExternalParticipant::currentId = 0;
+int ExternalParticipant::nextId() {
+    return ExternalParticipant::currentId++;
+}
+
+void ExternalParticipant::printSelf() const {
+    Participant::printSelf();
+    std::cout << "Came from " << this->originUniversity << " university." << std::endl;
+}
+
+std::string ExternalParticipant::originUniversityFromInput() {
+    std::string originUniversity;
+    std::cin >> originUniversity;
+    return originUniversity;
+}
